cdd6dda632abdc184c1d59804a74b59691bd0744
Michael Swanson <mikeswanson12@msn.com>
2013-08-28 15:30:45 -0400

    log all games, give winners

--- phase_4_kickball.rb
+++ phase_4_kickball.rb
@@ -1,10 +1,22 @@
-def identify_winner(hash, team_1, team_2)
- if hash[:team_1]>hash[:team_2]
-  puts"~~~~~~~~~~~#{team_1} wins!~~~~~~~~~~~~"
- else
-  puts"~~~~~~~~~~#{team_2} wins!~~~~~~~~~~~~~~"
-  puts""
- end
+def identify_winner(array)
+ m=0#team_1
+ i=1#score_1
+ v=2#team_2
+ z=3#score_2
+ h=0#Game num - 1
+ while array[h] != nil
+   if array[h][i]>array[h][z]
+    puts"~~~~ In Game #{h+1}, #{array[h][0]} won!~~~~~~~~~~~~"
+   else
+    puts"~~~~~In Game #{h+1}, #{array[h][2]} won!~~~~~~~~~~~~~~"
+    puts""
+   end
+   h+=1
+   # m+=4
+   # i+=4
+   # v+=4
+   # z+=4
+  end
 end
 
 def get_scores
@@ -26,15 +38,15 @@ def get_scores
     puts"What is team two's score?"
     puts""
     score_2 = gets.chomp.to_i
+    puts""
 
     scores_array=[]
-    scores_array[0] = team_1
-    scores_array[1] = score_1
-    scores_array[2] = team_2
-    scores_array[3] = score_2
+    scores_array<< team_1
+    scores_array<< score_1
+    scores_array<< team_2
+    scores_array<< score_2
 
-    log_array[i]<<scores_array
-    puts log_array
+    log_array<<scores_array
 
     puts"would you like to enter more game results?"
     puts"(y/n)"
@@ -42,7 +54,7 @@ def get_scores
     i+=1
     response = gets.chomp
     if response == "n"
-      # identify_winner(hash, team_1, team_2)
+      identify_winner(log_array)
       break
     end
   end
